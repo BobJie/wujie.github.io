371d8b1c1e95420def5ed0f3feb781ed7a27bac6
BobJack <607wujie2005@163.com>
2021-12-02 21:05:04 +0800

    modification at 20211202

--- data/data.js
+++ data/data.js
@@ -5,35 +5,35 @@ var dataForJournalt = [
     paper_title: "AutoBRB: An Automated Belief Rule Base Model for Pathologic Complete Response Prediction in Gastric Cancer",
     journel: "Computers in Biology and Medicine",
     time: "(Accepted).",
-    type: "(中科院SCI三区)"
+    type: "(中科院SCI三区，JCR Q2)"
   },
   {
     names: "X. Zhang, W. Ma, C. Li, J. Wu, X. Tang, L. Jiao",
     paper_title: "Fully Convolutional Network-Based Ensemble Method for Road Extraction from Aerial Images",
     journel: "IEEE Geoscience and Remote Sensing Letters",
     time: "2019, 17(10): 1777-1781.",
-    type: "(中科院SCI三区)"
+    type: "(中科院SCI三区, JCR Q1)"
   },
   {
     names: "F. Liu, J. Wu, LL Li, L.C. Jiao, H.X. Hao, X.R. Zhang",
     paper_title: "A Hybrid Method of SAR Speckle Reduction Based on Geometric-Structural Block and Adaptive Neighborhood",
     journel: "IEEE Transactions on Geoscience and Remote Sensing",
     time: "2018, 53(9): 5751-5764.",
-    type: "(中科院SCI二区)"
+    type: "(中科院SCI二区, JCR Q1)"
   },
   {
     names: "Y.P. Duan, F. Liu, L.C. Jiao, X.M. Tao, J. Wu, C. Shi, M. O. Wimmers",
     paper_title: "Adaptive Hierarchical Multinomial Latent Model with Hybrid Kernel Function for SAR Image Semantic Segmentation",
     journel: "IEEE Transactions on Geoscience and Remote Sensing",
     time: "2018, 56(10):5997-6015.",
-    type: "(中科院SCI二区)"
+    type: "(中科院SCI二区, JCR Q1)"
   },
   {
     names: "J. Wu, F. Liu, H.X. Hao, L.L. Li, L.C. Jiao, X.R. Zhang",
     paper_title: "Non-local Means for Speckle Reduction of SAR Image with Multiscale-Fusion-Based Steerable Kernel Function.",
     journel: "IEEE Geoscience and Remote Sensing Letters",
     time: "2016, 13(11): 1646-1650.",
-    type: "(中科院SCI三区)"
+    type: "(中科院SCI三区, JCR Q2)"
   },
 
   {
@@ -41,21 +41,21 @@ var dataForJournalt = [
     paper_title: "SAR image segmentation based on hieratical visual semantic and adaptive neighborhood multinomial latent model",
     journel: "IEEE Transactions on Geoscience and Remote Sensing",
     time: "2016, 54(7):4287-4301.",
-    type: "(中科院SCI二区)"
+    type: "(中科院SCI二区, JCR Q1)"
   },
   {
     names: "F. Liu, J.F. Shi, L.C. Jiao, H.Y. Liu, S.Y. Yang, J. Wu, H.X. Hao, J.L. Yuan",
     paper_title: "Hierarchical Semantic Model and Scattering Mechanism Based Pol-SAR Image Classification",
     journel: "Pattern Recognition",
     time: "2016, 59:325-342.",
-    type: "(中科院SCI二区)"
+    type: "(中科院SCI二区, JCR Q1)"
   },
   {
     names: "J. Wu, F. Liu, L.C. Jiao, X.R. Zhang, H.X. Hao, S. Wang",
     paper_title: "Local Maximal Homogeneous Region Search for SAR Speckle Reduction with Sketch-based Geometrical Kernel Function. ",
     journel: "IEEE Transactions on Geoscience and Remote Sensing",
     time: "2014, 53(9): 5751-5764.",
-    type: "(中科院SCI二区)"
+    type: "(中科院SCI二区, JCR Q1)"
   },
   {
     names: "马苗，许西丹，武杰",
