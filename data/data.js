80949d6714e24e7057c8f3226f6e306ddb289f0f
BobJack <607wujie2005@163.com>
2022-01-12 00:38:30 +0800

    paper change

--- data/data.js
+++ data/data.js
@@ -4,7 +4,7 @@ var dataForJournalt = [
     names: "J. Wu, Q. Wang, Z. Wang, Z. Zhou",
     paper_title: "AutoBRB: An Automated Belief Rule Base Model for Pathologic Complete Response Prediction in Gastric Cancer",
     journel: "Computers in Biology and Medicine",
-    time: "140(2022),105104.",
+    time: "2022，140,105104.",
     type: "(SCI, IF=4.589)"
   },
   {
